ecfe83ad86415b8f199960092a0e5cca1d3a73d8
Alex-Weatherhead <22604743+Alex-Weatherhead@users.noreply.github.com>
2019-04-01 09:53:46 -0400

    Updated build.

---
+++ build/lib/riot_api/api/get/endpoints/__init__.py
@@ -0,0 +1,9 @@
+import json
+
+_ENDPOINTS_V3_FILENAME = "./riot_api/api/get/endpoints/endpoints_v3.json"
+with open(_ENDPOINTS_V3_FILENAME, "r") as fp:
+    v3 = json.load(fp)
+
+_ENDPOINTS_V4_FILENAME = "./riot_api/api/get/endpoints/endpoints_v4.json"
+with open(_ENDPOINTS_V4_FILENAME, "r") as fp:
+    v4 = json.load(fp)
